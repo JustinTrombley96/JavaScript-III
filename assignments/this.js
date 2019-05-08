ea2c207d57045db206c88a1c273d1f4e7782e24e
Justin Trombley <jtrombley96@gmail.com>
2019-05-08 19:09:43 -0400

    Updated

--- assignments/this.js
+++ assignments/this.js
@@ -1,13 +1,13 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. This causes the value to be the window/console Object.
-* 2. When a function is called by a preceding dot, the object before that dot is "this".
-* 3. This is when a constructor is used, a specific instance of the object that is created and returned by the constructor function.
-* 4. Whenever JS's call or apply method is used.
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. This causes the value to be the window/console Object.
+ * 2. When a function is called by a preceding dot, the object before that dot is "this".
+ * 3. This is when a constructor is used, a specific instance of the object that is created and returned by the constructor function.
+ * 4. Whenever JS's call or apply method is used.
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 // code example for Window Binding
@@ -37,8 +37,8 @@
 function SuperHero(hero) {
     this.heroGreeting = "I'm here to save the day, ";
     this.hero = hero;
-    this.power = function() {
-        return(this.heroGreeting + this.hero);
+    this.power = function () {
+        return (this.heroGreeting + this.hero);
     };
 
 }
@@ -59,9 +59,9 @@ console.log(tonyStark.power());
 
 function person(attributes) {
     this.name = attributes,
-    this.tagLine = function () {
-        return `Hey, my name is ${this.name}`;
-    }
+        this.tagLine = function () {
+            return `Hey, my name is ${this.name}`;
+        }
 }
 
 const Lillian = new person('Lilly');
@@ -72,5 +72,3 @@ console.log(Bazill.tagLine());
 
 console.log(Lillian.tagLine.apply(Bazill));
 console.log(Bazill.tagLine.apply(Lillian));
-
-
